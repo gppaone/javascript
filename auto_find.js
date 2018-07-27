6cba3cd4a8b5241ab9145ecacf2895596874fb55
gppaone <gpaone@gmail.com>
2018-07-27 08:47:50 -0400

    New Functionality
    Added auto_find.js

---
+++ auto_find.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    // search field - on keyup
+    $('#search').on('keyup', function(){
+        console.log("working");
+        var searchTerm = $(this).val().toLowerCase();
+        // list to be filtered as characters are typed
+        $('.buildinglist button').each(function(){
+            var lineStr = $(this).text().toLowerCase();
+            if(lineStr.indexOf(searchTerm) === -1){
+                $(this).hide();
+            }else{
+                $(this).show();
+            }
+        });
+    });
+});
